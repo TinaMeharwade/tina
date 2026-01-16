f94aedd951e7f42537989f7ced94a4780216d279
TinaMeharwade <tinameharwade2001@gmail.com>
2026-01-17 01:10:52 +0530

    1D array problems

--- Arrays/OneDProblems.java
+++ Arrays/OneDProblems.java
@@ -48,5 +48,168 @@ public class OneDProblems {
         }
 
         int[] c = {0,1,2,3};
+
+//        Find the sum of all elements in an array.
+        int[] elements = new int[]{10,40,13,3,20};
+        int sum = 0;
+        for(int ele : elements){
+            sum = sum + ele;
+        }
+        System.out.println("sum of all elements in an array : " + sum);
+
+//        Find the average of array elements.
+        double avg = 0.0;
+        int sum2 = 0;
+        for (int e : elements){
+            sum2=sum2+e;
+        }
+        avg = (double) sum2/(elements.length);
+        System.out.println("average of array elements : " + avg);
+
+//        Count how many numbers are even and odd.
+        int evenCnt = 0, oddCnt = 0;
+        for(int e : elements){
+            if((e%2)==0)
+            {
+                evenCnt++;
+            } else {
+                oddCnt++;
+            }
+        }
+        System.out.println("Even numbers count : " + evenCnt);
+        System.out.println("Odd numbers count : " + oddCnt);
+
+
+//        Find the largest element in an array.
+        // int[] elements = new int[]{10,40,13,3,20};
+        int largest = elements[0];
+        for(int i=1;i<elements.length;i++){
+            if(elements[i]>largest)
+            {
+                largest=elements[i];
+            }
+        }
+        System.out.println("largest element in array : " + largest);
+
+//        Find the smallest element in an array.
+        int smallest = elements[0];
+        for(int i=1;i<elements.length;i++){
+            if(elements[i]<smallest)
+            {
+                smallest=elements[i];
+            }
+        }
+        System.out.println("smallest element in array : " + smallest);
+
+//        Reverse an array.
+        int[] array1 = new int[]{1,2,3,10,4,5};
+        int start=0;
+        int end = array1.length-1;
+        while(start < end)
+        {
+            int temp = array1[start];
+            array1[start]=array1[end];
+            array1[end]=temp;
+
+            start++;
+            end--;
+
+        }
+        System.out.println("reversed array : ");
+        for(int arr : array1)
+        {
+            System.out.print(arr+" ");
+        }
+
+//        Copy elements from one array to another.
+        System.out.println("\nCopy elements from one array to another : " );
+        System.out.println("Original array : " );
+        for(int temp : array1){
+            System.out.print(temp+" ");
+        }
+        // int[] array1 = new int[]{1,2,3,10,4,5};
+        int[] array2 = new int[array1.length];
+        for(int i=0;i<array1.length;i++)
+        {
+            array2[i]=array1[i];
+        }
+        System.out.println("\nCopied array : ");
+        for(int temp : array2){
+            System.out.print(temp+" ");
+        }
+
+//        Check if a given element exists in the array (linear search).
+        // 5 4 10 3 2 1
+        int toSearch=1;
+        int flag=0;
+        for (int j : array1) {
+            if (toSearch == j) {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag==1)
+        {
+            System.out.print("\n"+toSearch + " is element found in array");
+        }else {
+            System.out.print("\n"+toSearch + " is element that does not exist in array");
+
+        }
+
+//        Count how many times a number appears in the array.
+        int[] arrnum = new int[]{5,1,3,8,1,5,7,5,3};
+        int toCnt=5;
+        int cnt=0;
+        for(int i : arrnum)
+        {
+            if(toCnt==i)
+            {
+                cnt++;
+            }
+        }
+        System.out.println("\nThe number " + toCnt + " appears " + cnt + " times in givan array");
+
+//        Replace all negative numbers with 0.
+        int[] arr3 = new int[]{0,-3,2,3,8,-8,-1,-9,6,-4,8,-1};
+        System.out.println("Before replacing -ve numbers with 0 array is : ");
+        for(int i : arr3)
+        {
+            System.out.print(" " + i);
+        }
+
+//        In a for-each loop, k is a copy of the value. Changing k does NOT change the array
+//        So even though you write k = 0, the actual array arr3 remains unchanged. Hence, for each loop won't work
+//        for(int k : arr3)
+//        {
+//            if(k<0)
+//            {
+//                k=0;
+//            }
+//        }
+//        What Java actually does internally (conceptually):
+//        int[] arr = {1, 2, 3};
+//        for (int x : arr) {
+//            x = 0;
+//        }
+//        for (int i = 0; i < arr.length; i++) {
+//            int x = arr[i];   // COPY of value
+//            x = 0;            // changes only the copy
+//        }
+//        x is just a temporary variable
+//        Changing x does NOT change arr[i]
+        for(int k=0;k<arr3.length;k++)
+        {
+            if(arr3[k]<0)
+            {
+                arr3[k]=0;
+            }
+        }
+        System.out.println("\nAfter replacing -ve numbers with 0 array is : ");
+        for(int i : arr3)
+        {
+            System.out.print(" " + i);
+        }
     }
 }
+
+//https://github.com/TinaMeharwade/tina/tree/Array/Arrays
